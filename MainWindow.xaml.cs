a6beabe0fe041dc9a8e46a79443b8a1a94d9912c
NikolaiSaharov <145434551+NikolaiSaharov@users.noreply.github.com>
2024-03-20 12:17:54 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PRAKTIKA4entity
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private AVIACASSA2Entities context = new AVIACASSA2Entities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            PassengerGrid.ItemsSource = context.Passengers.ToList();
+            FiltrName.ItemsSource = context.Passengers.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+
+            // Закройте текущее окно
+            this.Close();
+        }
+
+        private void SearchClick_Click(object sender, RoutedEventArgs e)
+        {
+            PassengerGrid.ItemsSource = context.Passengers.ToList().Where(item => item.FirstName.Contains(SearchName.Text));
+        }
+
+        private void ClearClick_Click(object sender, RoutedEventArgs e)
+        {
+            PassengerGrid.ItemsSource = context.Passengers.ToList();
+        }
+
+        private void FiltrName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FiltrName.SelectedItem != null)
+            {
+                var selected = FiltrName.SelectedItem as Passengers;
+                PassengerGrid.ItemsSource = context.Passengers.ToList().Where(item => item.FirstName == selected.FirstName).ToList();
+            }
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PRAKTIKA4entity
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private AVIACASSA2Entities context = new AVIACASSA2Entities();
+        public Window1()
+        {
+            InitializeComponent();
+            TicketGrid.ItemsSource = context.Tickets.ToList();
+            FiltrClass.ItemsSource = context.Tickets.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+
+            // Закройте текущее окно
+            this.Close();
+        }
+
+        private void SearchClick_Click(object sender, RoutedEventArgs e)
+        {
+            TicketGrid.ItemsSource = context.Tickets.ToList().Where(item => item.Class.Contains(SearchClass.Text));
+        }
+
+        private void ClearClick_Click(object sender, RoutedEventArgs e)
+        {
+            TicketGrid.ItemsSource = context.Tickets.ToList();
+        }
+
+        private void FiltrName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FiltrClass.SelectedItem != null)
+            {
+                var selected = FiltrClass.SelectedItem as Tickets;
+                TicketGrid.ItemsSource = context.Tickets.ToList().Where(item => item.Class == selected.Class).ToList();
+            }
+        }
+    }
+}
